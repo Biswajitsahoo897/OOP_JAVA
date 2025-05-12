0b3b1f2e7dbbdc2d7a1a05d3d99fb9c478f36748
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-05-12 22:16:00 +0530

    Added variable a and b

--- MAIN.java
+++ MAIN.java
@@ -5,6 +5,9 @@ public class MAIN {
         System.out.println("Printing from static method");
     }
     public static void main(String[] args) {
+        int a=90;
+        int b=80;
+        System.out.println("Sum is "+(a+b));
         System.out.println("Hello World");
     }
 }

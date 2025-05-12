d3b0ac690110fe8f82ae668ec2da27badba27fbe
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-05-12 22:23:45 +0530

    Revert "Added variable a and b"
    This reverts commit 0b3b1f2e7dbbdc2d7a1a05d3d99fb9c478f36748.

--- MAIN.java
+++ MAIN.java
@@ -5,9 +5,6 @@ public class MAIN {
         System.out.println("Printing from static method");
     }
     public static void main(String[] args) {
-        int a=90;
-        int b=80;
-        System.out.println("Sum is "+(a+b));
         System.out.println("Hello World");
         show();
     }

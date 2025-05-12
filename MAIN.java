81c57a49cf939a6fc1a61f33f275a5242cf2cb44
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-05-12 22:11:11 +0530

    Added the show method

--- MAIN.java
+++ MAIN.java
@@ -1,6 +1,9 @@
 package TestGit;
 
 public class MAIN { 
+    static void show(){
+        System.out.println("Printing from static method");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
     }

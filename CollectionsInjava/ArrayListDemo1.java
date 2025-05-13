9c0769209830b2c8bba88aa49ba1f23e644cd3af
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-05-14 00:42:43 +0530

    Added ArrayListDemo1.java

---
+++ CollectionsInjava/ArrayListDemo1.java
@@ -0,0 +1,16 @@
+package CollectionsInjava;
+import java.util.*; 
+public class ArrayListDemo1 {
+    public static void main(String[] args) {
+        // Create an ArrayList
+        ArrayList list = new ArrayList<>();
+        System.out.println("ArrayList size:(BEFORE) " + list.size());
+        // Add elements to the ArrayList
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        System.out.println("ArrayList size:(AFTER) " + list.size());
+
+
+    }
+}
